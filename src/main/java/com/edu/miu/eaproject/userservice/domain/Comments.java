b522935fdfbd248cee6eee7c127cd2ebe9c789a0
Demelash Minyahil <dememan@gmail.com>
2022-01-29 11:09:13 -0600

    remove post & comments models

--- src/main/java/com/edu/miu/eaproject/userservice/domain/Comments.java
+++
@@ -1,6 +0,0 @@
-package com.edu.miu.eaproject.userservice.domain;
-
-public class Comments {
-    private Long id;
-    private String body;
-}
--- src/main/java/com/edu/miu/eaproject/userservice/domain/Post.java
+++
@@ -1,8 +0,0 @@
-package com.edu.miu.eaproject.userservice.domain;
-
-public class Post {
-    private Long id;
-    private String title;
-    private String body;
-    private int likes;
-}
